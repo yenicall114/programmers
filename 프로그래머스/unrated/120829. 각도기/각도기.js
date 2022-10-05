2b1761a02f78250095811f0245479f8362706557
yenicall <104622110+yenicall114@users.noreply.github.com>
2022-10-05 20:08:29 +0900

    [unrated] Title: 각도기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,11 @@
+function solution(angle) {
+    
+    if (angle === 180) return 4
+    
+    if (angle > 90 ) return 3
+    
+    if (angle === 90) return 2
+    
+    if (angle < 90) return 1
+}
+
