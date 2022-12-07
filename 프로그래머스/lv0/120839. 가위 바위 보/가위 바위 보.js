1db82f26f711465bbe82bb2531cdaef62d015f78
yenicall <104622110+yenicall114@users.noreply.github.com>
2022-12-08 01:49:54 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,16 +1,9 @@
 function solution(rsp) {
-    const arr = [...rsp]
-    const result = []
-    
-    for (let i=0; i<arr.length; i++) {
-        if (arr[i] === "2") {
-            result.push("0")
-        } else if (arr[i] === "0") {
-            result.push("5")
-        } else {
-            result.push("2")
-        }
-        console.log(result.join(""))
+    const result = {
+        2: 0,
+        0: 5,
+        5: 2,
     }
-    return result.join("")
+    
+    return [...rsp].map((el) => result[el]).join("")
 }
