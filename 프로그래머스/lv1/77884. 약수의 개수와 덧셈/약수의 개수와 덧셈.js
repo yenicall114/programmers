98d946dd7dad2cacc29a20790fd9234c3584768b
yenicall <104622110+yenicall114@users.noreply.github.com>
2022-12-22 02:28:41 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,19 @@
+function solution(left, right) {
+    let result = 0;
+    
+    for (let i=left; i<=right; i++) {    
+        let count = 0;
+        
+        for (let j=1; j<=right; j++) {
+            if (i % j === 0) {
+                count++
+            }
+        }
+        if (count % 2 === 0) {
+            result += i
+        } else {
+            result -= i
+        }
+    }
+    return result
+}
