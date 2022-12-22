803a8440a5a6f403bf93ccca626dcf4547ffb58e
yenicall <104622110+yenicall114@users.noreply.github.com>
2022-12-23 03:30:28 +0900

    [level 0] Title: 가까운 수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,9 +1,4 @@
 function solution(array, n) {
-    const arr = array.sort((a, b) => a - b).map((el) => el - n)
-
-    for (let i=0; i<arr.length; i++) {
-      if (arr[i] < 0) arr[i] *= -1
-    }
-    
+    const arr = array.sort((a, b) => a - b).map(el => Math.abs(el - n))
     return array[arr.indexOf(Math.min(...arr))]
 }
