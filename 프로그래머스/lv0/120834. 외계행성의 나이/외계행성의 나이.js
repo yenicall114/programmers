89be0630a0a1b650177decd5d2849ddf22b33d4d
yenicall <104622110+yenicall114@users.noreply.github.com>
2022-12-30 10:25:30 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,16 +1,3 @@
 function solution(age) {
-    const str = {
-        0: "a",
-        1: "b",
-        2: "c",
-        3: "d",
-        4: "e",
-        5: "f",
-        6: "g",
-        7: "h",
-        8: "i",
-        9: "j",
-    }
-    
-    return [..."" + age].map((el) => str[el]).join("")
+    return [..."" + age].map((el) => "abcdefghij"[el]).join("")
     }
