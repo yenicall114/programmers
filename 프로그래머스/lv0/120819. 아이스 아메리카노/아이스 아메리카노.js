a5bf87c2fb31dbb5495034c07b9698734094c189
yenicall <104622110+yenicall114@users.noreply.github.com>
2023-01-06 20:03:25 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,8 @@
 function solution(money) {
-    const americano = 5500
-    const a = Math.floor(money / americano)
-    const b = money - americano * a
-    return [a, b];
+    const result = []
+    
+    result.push(Math.floor(money / 5500))
+    result.push(money - 5500 * Math.floor(money / 5500))
+
+    return result
 }
