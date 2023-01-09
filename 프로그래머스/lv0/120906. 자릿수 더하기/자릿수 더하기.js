4483ca738562c55b3be9cfe4303b51bd39c0f7a3
yenicall <104622110+yenicall114@users.noreply.github.com>
2023-01-09 21:25:23 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,3 @@
 function solution(n) {
-    var answer = 0;
-    answer = n.toString().split("").map(Number).reduce((acc, cur) => acc + cur);
-    return answer;
+    return [...n.toString()].map(Number).reduce((a, c) => a + c)
 }
